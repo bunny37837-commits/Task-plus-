4039a97a01bbbe4cffde52f9bfaacedef113b3fd
bootai134u-ux <bootai134u@gmail.com>
2026-03-10 17:31:34 +0530

    fix: remove unused AddTaskViewModel2

--- app/src/main/java/com/taskpulse/app/presentation/addtask/AddTaskViewModel2.kt
+++
@@ -1,6 +0,0 @@
-// Extension to fix uiState reference
-package com.taskpulse.app.presentation.addtask
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.StateFlow
-// uiState alias for AddTaskViewModel
-val AddTaskViewModel.uiState: StateFlow<AddTaskUiState> get() = state
